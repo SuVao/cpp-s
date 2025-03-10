3a89ed24af25da20081bc5297776bae25d3daefa
SuVao <pedro.radon@gmail.com>
2025-03-10 14:25:54 +0000

    cpp01 ex01

--- cpp01/ex00/main.cpp
+++ cpp01/ex00/main.cpp
@@ -3,4 +3,13 @@
 
 int main()
 {
+    Zombie *Zombie_heap;
+    std::string input;
+
+    std::cout << "Give me a name!" << std::endl;
+    std::cin >> input;
+    Zombie_heap = newZombie(input + " heap");
+    Zombie_heap->announce();
+    randomChump(input + " stack");
+    delete Zombie_heap;
 }
