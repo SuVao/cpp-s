a2881aa9c55f6da8d905fad4bb884f0e06939710
SuVao <pedro.radon@gmail.com>
2025-03-15 15:57:28 +0000

    s

--- cpp01/ex03/HumanA.hpp
+++ cpp01/ex03/HumanA.hpp
@@ -1,8 +1,6 @@
 
 #ifndef HUMANA_HPP
-#define HUMANA.HPP
+#define HUMANA_HPP
 
-#include "HumanB.hpp"
-#include "Weapon.hpp"
 
 #endif
--- cpp01/ex03/HumanB.hpp
+++ cpp01/ex03/HumanB.hpp
@@ -2,7 +2,5 @@
 #ifndef HUMANB_HPP
 #define HUMANB_HPP
 
-#include "HumanA.hpp"
-#include "Weapon.hpp"
 
 #endif
