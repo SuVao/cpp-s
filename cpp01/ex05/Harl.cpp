cf10a7a560c9d560513c992c3f38cf1a64d4d811
SuVao <pedro.radon@gmail.com>
2025-03-19 20:32:08 +0000

    ex05

--- cpp01/ex05/Harl.cpp
+++ cpp01/ex05/Harl.cpp
@@ -1,40 +1,44 @@
 
 #include "Harl.hpp"
 
-//void    Harl::debug( void )
-//{
-//    std::cout << "I love having extra bacon for my 7XL-double-cheese-triple-pickle-specialketchup burger. I really do!" << std::endl;
-//}
-
 void    Harl::info( void )
 {
+	std::cout << "[INFO]" << std::endl;
     std::cout << "I cannot believe adding extra bacon costs more money. You didn’t put \
     enough bacon in my burger! If you did, I wouldn’t be asking for more!" << std::endl;
 }
 
 void    Harl::warning( void )
 {
+	std::cout << "[WARNING]" << std::endl;
     std::cout <<  "I think I deserve to have some extra bacon for free. I’ve been coming for \
     years, whereas you started working here just last month." << std::endl;
 }
 
 void Harl::debug( void )
 {
+	std::cout << "[DEBUG]" << std::endl;
     std::cout << "I love having extra bacon for my 7XL-double-cheese-triple-pickle-specialketchup burger. I really do!" << std::endl;
 }
 
 void    Harl::error( void )
 {
+	std::cout << "[ERROR]" << std::endl;
     std::cout <<  "This is unacceptable! I want to speak to the manager now." << std::endl;
 }
 
 void    Harl::complain( std::string level )
 {
-    std::string level1[4] = { "WARNING", "DEBUG", "INFO", "ERROR" };
-
+    std::string level1[] = { "WARNING", "DEBUG", "INFO", "ERROR" };
+	void (Harl::*functions[])() = { &Harl::warning, &Harl::debug, &Harl::info, &Harl::error};
+	
     for (int i = 0; i < 4; i++)
-
-
-
-    level1->(Harl::debug());
+	{
+		if (level1[i] == level)
+		{
+			(this->*functions[i])();
+			return ;
+		}
+	}
+	std::cout << "Level not founded!" << std::endl;
 }
--- cpp01/ex05/main.cpp
+++ cpp01/ex05/main.cpp
@@ -3,5 +3,10 @@
 
 int main()
 {
+	Harl ola;
 
+	ola.complain("WARNING");
+	ola.complain("DEBUG");
+	ola.complain("INFO");
+	ola.complain("ERROR");
 }
