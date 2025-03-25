b1a3af84ffe77c4aefe0da97583d0fe154ccaef3
SuVao <pedro.radon@gmail.com>
2025-03-25 10:37:28 +0000

    ex00 fixed

---
+++ cpp01/ex00/Zombie.cpp
@@ -0,0 +1,22 @@
+
+#include "Zombie.hpp"
+
+Zombie::Zombie(std::string input)
+{
+    name = input;
+}
+
+Zombie::~Zombie(void)
+{
+    std::cout << name << " foi destruído!" << std::endl;
+}
+
+std::string Zombie::get_name(void)
+{
+	return (name);
+}
+
+void	Zombie::announce( void )
+{
+	std::cout << get_name() << ": " << "BraiiiiiiinnnzzzZ..." << std::endl;
+}
--- cpp01/ex00/newZombie.cpp
+++ cpp01/ex00/newZombie.cpp
@@ -1,26 +1,6 @@
 
 #include "Zombie.hpp"
 
-Zombie::Zombie(std::string input)
-{
-    name = input;
-}
-
-Zombie::~Zombie(void)
-{
-    std::cout << name << " foi destruído!" << std::endl;
-}
-
-std::string Zombie::get_name(void)
-{
-	return (name);
-}
-
-void	Zombie::announce( void )
-{
-	std::cout << get_name() << ": " << "BraiiiiiiinnnzzzZ..." << std::endl;
-}
-
 Zombie* newZombie(std::string name)
 {
 	Zombie *new_z = new Zombie(name);
