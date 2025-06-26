6b73be5f7eaf38c8e7656d7e4c592c2082545461
SuVao <pedro.radon@gmail.com>
2025-06-26 16:14:52 +0100

    cpp07 dome

--- cpp07/ex02/inc/Array.hpp
+++ cpp07/ex02/inc/Array.hpp
@@ -6,16 +6,55 @@
 
 #include <string>
 #include <iostream>
+#include <exception>
 
 template <typename T>
 class Array
 {
 private:
-	T a;
+	T* a;
+	size_t size1;
 public:
-	Array();
-	Array(const unsigned int a);
-	Array& operator=();
+	~Array() { delete[] a ;};
+
+	Array() : a(NULL), size1(0) {};
+
+	Array(size_t b) : a(new T[b]), size1((b)) {};
+
+	Array& operator=(const Array& other)
+	{
+		delete[] a;
+		size1 = other.size1;
+		a = new T[size1];
+		for (size_t i = 0; i < other.size1; i++)
+			a[i] = other.a[i];
+		return *this;
+	};
+
+	Array(const Array& other) : a(new T[other.size1]), size1(other.size1)
+	{
+		for (size_t i = 0; i < size1; i++)
+			a[i] = other.a[i];
+	};
+
+	T& operator[](std::size_t index)
+	{
+	    if (index >= size1)
+	        throw std::out_of_range("Index out of bounds");
+	    return a[index];
+	}
+
+	const T& operator[](std::size_t index) const
+	{
+	    if (index >= size())
+	        throw std::out_of_range("Index out of bounds");
+	    return a[index];
+	}
+
+	size_t size() const
+	{
+		return size1;
+	}
 };
 
 #endif
--- cpp07/ex02/src/main.cpp
+++ cpp07/ex02/src/main.cpp
@@ -1,2 +1,33 @@
 
-#include 
+#include "../inc/Array.hpp"
+
+int main() {
+    try {
+        Array<int> a(9);
+
+        for (size_t i = 0; i < a.size(); ++i) 
+            a[i] = static_cast<int>(i * 10);
+
+        for (size_t i = 0; i < a.size(); ++i) {
+            std::cout << "a[" << i << "] = " << a[i] << std::endl;
+        }
+
+        std::cout << "Tentando acessar a[10]..." << std::endl;
+        std::cout << a[10] << std::endl;  
+
+
+    } catch (const std::out_of_range& e) {
+        std::cerr << "Exceção capturada: " << e.what() << std::endl;
+    }
+
+	try {
+   		const Array<int> b(3);
+		size_t index = 2;
+		std::cout << "Acessando array const: b[" << index << "] = " << b[index] << std::endl;
+	} catch (const std::out_of_range& e) {
+        std::cerr << e.what() << std::endl;
+    }
+
+
+    return 0;
+}
